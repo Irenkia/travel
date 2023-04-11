437c1f075f122b7e148ffebe25110fd65ac86804
Iryna Tiutiun <tiutiuniryna@gmail.com>
2023-04-11 19:52:53 +0200

    changed photos

--- src/components/cards/Cards.js
+++ src/components/cards/Cards.js
@@ -1,27 +1,27 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { CARDS } from '../../constants/Cards';
+import React from "react";
+import { Link } from "react-router-dom";
+import { CARDS } from "../../constants/Cards";
 
-import './Cards.css';
+import "./Cards.css";
 
 function Cards() {
   return (
-    <div className='cards'>
+    <div className="cards">
       <h1>Check out these EPIC Destinations!</h1>
-      <div className='cards-container'>
+      <div className="cards-container">
         {CARDS.map((item) => {
-              return (
-                <div key={item.id} className="cards-item">
-                  <Link className='cards-item-link' to={item.path}>
-                  <div className='card-item-img'>{item.img}</div>
-                  <div className='cards-item-info'>
-                  <p className='cards-item-text'>{item.text}</p>
+          return (
+            <div key={item.id} className="cards-item">
+              <Link className="cards-item-link" to={item.path}>
+                <div className="card-item-img">{item.img}</div>
+                <div className="cards-item-info">
+                  <p className="cards-item-text">{item.text}</p>
                   <h2>{item.label}</h2>
-                  </div>
-                  </Link>
                 </div>
-              );
-            })}
+              </Link>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
--- src/constants/Cards.js
+++ src/constants/Cards.js
@@ -1,51 +1,51 @@
-import waterfall from './../assets/waterfall1.jpg';
-import Bali from './../assets/Bali.jpg';
-import islands from './../assets/islands.jpg';
-import Himalayas from './../assets/Himalayas.jpg';
-import desert from './../assets/desert.jpg';
-import Europe from './../assets/Europe.png';
+import vodopad from "./../assets/vodopad.jpg";
+import Bali2 from "./../assets/Bali2.jpg";
+import islands2 from "./../assets/islands2.jpg";
+import Himalayas2 from "./../assets/Himalayas2.jpg";
+import desert2 from "./../assets/desert2.jpg";
+import Europe from "./../assets/Europe.png";
 
-export const CARDS =[
-    {
-        id: 1,
-        img: <img src={waterfall} alt="Travel Img" />,
-        text: 'Explore the hidden waterfall deep inside the Amazon Jungle',
-        label: 'Adventure',
-        path: '/dashboard', 
-    },
-    {
-        id: 2,
-        img: <img src={Bali} alt="Travel Img" />,
-        text: 'Travel through the Islands of Bali in a Private Cruisee',
-        label: 'Luxury',
-        path: '/dashboard', 
-    },
-    {
-        id: 3,
-        img: <img src={islands} alt="Travel Img" />,
-        text: 'Explore the hidden waterfall deep inside the Amazon Jungle',
-        label: 'Mystery',
-        path: '/dashboard', 
-    },
-    {
-        id: 4,
-        img: <img src={Himalayas} alt="Travel Img" />,
-        text: 'Experience Football on Top of the Himilayan Mountains',
-        label: 'Adrenaline',
-        path: '/dashboard', 
-    },
-    {
-        id: 5,
-        img: <img src={desert} alt="Travel Img" />,
-        text: 'Ride through the Sahara Desert on a guided camel tour',
-        label: 'Adventure',
-        path: '/dashboard', 
-    },
-    {
-        id: 6,
-        img: <img src={Europe} alt="Travel Img" />,
-        text: 'Express in Europe - several countries in one trip',
-        label: 'Adventure',
-        path: '/dashboard', 
-    },
-]
+export const CARDS = [
+  {
+    id: 1,
+    img: <img src={vodopad} alt="Travel Img" />,
+    text: "Explore the hidden waterfall deep inside the Amazon Jungle",
+    label: "Adventure",
+    path: "/dashboard",
+  },
+  {
+    id: 2,
+    img: <img src={Bali2} alt="Travel Img" />,
+    text: "Travel through the Islands of Bali in a Private Cruisee",
+    label: "Luxury",
+    path: "/dashboard",
+  },
+  {
+    id: 3,
+    img: <img src={islands2} alt="Travel Img" />,
+    text: "Set Sail in the Atlantic Ocean visiting Uncharted Waters",
+    label: "Mystery",
+    path: "/dashboard",
+  },
+  {
+    id: 4,
+    img: <img src={Himalayas2} alt="Travel Img" />,
+    text: "Experience adrenaline on Top of the Himalayan Mountains",
+    label: "Adrenaline",
+    path: "/dashboard",
+  },
+  {
+    id: 5,
+    img: <img src={desert2} alt="Travel Img" />,
+    text: "Ride through the Sahara Desert on a guided camel tour",
+    label: "Adventure",
+    path: "/dashboard",
+  },
+  {
+    id: 6,
+    img: <img src={Europe} alt="Travel Img" />,
+    text: "Express in Europe - several countries in one trip",
+    label: "Adventure",
+    path: "/dashboard",
+  },
+];
