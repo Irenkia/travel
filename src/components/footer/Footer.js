9c5c423e7e93523efbc8493041b08cfd2d27502d
Iryna Tiutiun <tiutiuniryna@gmail.com>
2023-04-16 21:03:53 +0200

    added useState to forms in SignUp and Footer files

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,31 +1,49 @@
-import React from 'react';
-import { Button } from '../button/Button';
+import React, { useState } from "react";
+import { Button } from "../button/Button";
 import "./Footer.css";
 
 const Footer = () => {
-    return ( 
-        <div className='footer-container'>
-        <section className='footer-subscription'>
-          <p className='footer-subscription-heading'>
-            Join the Adventure newsletter to receive our best vacation deals
-          </p>
-          <p className='footer-subscription-text'>
-            You can unsubscribe at any time.
-          </p>
-          <div className='input-areas'>
-            <form>
-              <input
-                className='footer-input'
-                name='email'
-                type='email'
-                placeholder='Your Email'
-              />
-              <Button buttonStyle='btn--outline' path='./about'>Subscribe</Button>
-            </form>
-          </div>
-        </section>
-      </div>
-     );
-}
+  const [email, setEmail] = useState("");
+
+  const handleInputChange = (e) => {
+    const { value } = e.target;
+    setEmail(value);
+  };
+
+  const handleSubmit = () => {
+    console.log(email);
+  };
+  return (
+    <div className="footer-container">
+      <section className="footer-subscription">
+        <p className="footer-subscription-heading">
+          Join the Adventure newsletter to receive our best vacation deals
+        </p>
+        <p className="footer-subscription-text">
+          You can unsubscribe at any time.
+        </p>
+        <div className="input-areas">
+          <form>
+            <input
+              className="footer-input"
+              type="email"
+              value={email}
+              onChange={(e) => handleInputChange(e)}
+              name="email"
+              placeholder="Your Email"
+            />
+            <Button
+              onClick={() => handleSubmit()}
+              buttonStyle="btn--outline"
+              path="./about"
+            >
+              Subscribe
+            </Button>
+          </form>
+        </div>
+      </section>
+    </div>
+  );
+};
 
 export default Footer;
--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,9 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import Footer from "../components/footer/Footer";
 import "./../styles/App.css";
 import "./../styles/SignUp.css";
 
 const SignUp = () => {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const handleInputChange = (e) => {
+    const { id, value } = e.target;
+    if (id === "firstName") {
+      setFirstName(value);
+    }
+    if (id === "lastName") {
+      setLastName(value);
+    }
+    if (id === "email") {
+      setEmail(value);
+    }
+    if (id === "password") {
+      setPassword(value);
+    }
+    if (id === "confirmPassword") {
+      setConfirmPassword(value);
+    }
+  };
+
+  const handleSubmit = () => {
+    console.log(firstName, lastName, email, password, confirmPassword);
+  };
+
   return (
     <>
       <div className="sign-up">
@@ -11,64 +40,78 @@ const SignUp = () => {
         <div className="form">
           <div className="form-body">
             <div className="username">
-              <label className="form-label" for="firstName">
+              <label className="form-label" htmlFor="firstName">
                 First Name
               </label>
               <input
                 className="form-input"
                 type="text"
+                value={firstName}
+                onChange={(e) => handleInputChange(e)}
                 id="firstName"
                 placeholder="First Name"
               />
             </div>
             <div className="lastname">
-              <label className="form-label" for="lastName">
+              <label className="form-label" htmlFor="lastName">
                 Last Name
               </label>
               <input
+                className="form-input"
                 type="text"
+                value={lastName}
+                onChange={(e) => handleInputChange(e)}
                 name=""
                 id="lastName"
-                className="form-input"
                 placeholder="LastName"
               />
             </div>
             <div className="email">
-              <label className="form-label" for="email">
+              <label className="form-label" htmlFor="email">
                 Email
               </label>
               <input
-                type="email"
-                id="email"
                 className="form-input"
+                type="email"
+                value={email}
+                onChange={(e) => handleInputChange(e)}
+                id="email"
                 placeholder="Email"
               />
             </div>
             <div className="password">
-              <label className="form-label" for="password">
+              <label className="form-label" htmlFor="password">
                 Password
               </label>
               <input
                 className="form-input"
                 type="password"
+                value={password}
+                onChange={(e) => handleInputChange(e)}
                 id="password"
                 placeholder="Password"
               />
             </div>
             <div className="confirm-password">
-              <label className="form-label" for="confirmPassword">
+              <label className="form-label" htmlFor="confirmPassword">
                 Confirm Password
               </label>
               <input
                 className="form-input"
                 type="password"
+                value={confirmPassword}
+                onChange={(e) => handleInputChange(e)}
                 id="confirmPassword"
                 placeholder="Confirm Password"
               />
             </div>
           </div>
-          <div class="submit">
-            <button type="submit" class="btn">
+          <div className="submit">
+            <button
+              onClick={() => handleSubmit()}
+              type="submit"
+              className="btn"
+            >
               Register
             </button>
           </div>
