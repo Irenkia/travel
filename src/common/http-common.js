e893627e5a6d575070bfb79fc61d3f38dd3def0b
Iryna Tiutiun <tiutiuniryna@gmail.com>
2023-06-13 22:49:10 +0200

    added new headers

--- src/common/http-common.js
+++ src/common/http-common.js
@@ -1,10 +1,14 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:8080/api",
+  // baseURL: "http://localhost:8080/api",
+  // baseURL: "http://localhost:9191/api",
+  baseURL: "http://travel-server.database.windows.net/api",
   headers: {
+    Accept: "application/json",
     "Content-type": "application/json; charset=utf-8",
     "API-Key": "secret",
+    "Access-Control-Allow-Credentials": "*",
   },
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
   credentials: "include",
